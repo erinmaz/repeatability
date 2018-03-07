8097caf5a6c9e25cee37e419eb6c60e2a244f6d8
Erin Mazerolle <erinmaz@gmail.com>
2018-03-06 21:36:34 -0700

    commands to convert dicoms_anon to nifti

---
+++ dcm2niix_commands.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R02/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R02/dicom-R02_20130709_185233280
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R02/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R02/dicom-R02_20130715*
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R03
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R03/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R03/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R03/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R03/dicom-R03_20130718*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R03/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R03/dicom-R03_20130723*
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R05
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R05/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R05/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R05/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R05/dicom-R05_20130729*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R05/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R05/dicom-R05_20130801*
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R06
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R06/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R06/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R06/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R06/dicom-R06_20130802*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R06/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R06/dicom-R06_20130809*
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R07
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R07/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R07/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R07/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R07/dicom-R07_20130826*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R07/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R07/dicom-R07_20130829*
+
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R08
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R08/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R08/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R08/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R08/dicom-R08_20130912*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R08/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R08/dicom-R08_20130913*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R08/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R08/dicom-R08_20130919*
+
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R09
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R09/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R09/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R09/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R09/dicom-R09_20130919*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R09/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R09/dicom-R09_20130925*
+
+
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R10
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R10/sess1
+mkdir /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R10/sess2
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R10/sess1 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R10/dicom-R10_20130919*
+
+dcm2niix -f %d_s%s_e%e -o /Users/erin/Desktop/Projects/Repeatability/fresh_nii_March2018/R10/sess2 /Users/erin/Desktop/Projects/Repeatability/dicoms_anon/R10/dicom-R10_20130925*
