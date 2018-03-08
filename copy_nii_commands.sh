d569c1e115424926c33d7d4b9a82f6ea28b27bc8
Erin Mazerolle <erinmaz@gmail.com>
2018-03-08 13:31:48 -0700

    fixed typo in copy_nii_commands.sh

--- copy_nii_commands.sh
+++ copy_nii_commands.sh
@@ -37,18 +37,176 @@ fslmaths ${NIIDIR}/R02/sess2/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R02/sess2/nii/RS_
 fslmaths ${NIIDIR}/R02/sess2/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R02/sess2/nii/RS_e1
 fslmaths ${NIIDIR}/R02/sess2/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R02/sess2/T1
 
-#check which HC runs I should use (and which ones I used for the ET data!!!!)
+#For R03 session 1, use 2nd and 3rd HC runs. The first HC e1 data has a lot of negative activation according to my notes, and we acquired a third because we were worried the first one wouldn't look right (maybe because participant reported not following instructions?)
 fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R03/sess1/nii/BH1_e2
 fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R03/sess1/nii/BH1_e1
 fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_BH_s18_e2 ${MAINDIR}/R03/sess1/nii/BH2_e2
 fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_BH_s18_e1 ${MAINDIR}/R03/sess1/nii/BH2_e1
-fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_s8_e2 ${MAINDIR}/R03/sess1/nii/HC1_e2
-fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_s8_e1 ${MAINDIR}/R03/sess1/nii/HC1_e1
-fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_s12_e2 ${MAINDIR}/R03/sess1/nii/HC2_e2
-fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_s12_e1 ${MAINDIR}/R03/sess1/nii/HC2_e1
-fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_RS_s6_e2 ${MAINDIR}/R03/sess1/nii/RS_e2
-fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_RS_s6_e1 ${MAINDIR}/R03/sess1/nii/RS_e1
-fslmaths ${NIIDIR}/R03/sess1/MPRAGE_ADNI_Ipat2_s4_e1 ${MAINDIR}/R03/sess1/T1
+fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R03/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R03/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_2_s12_e2 ${MAINDIR}/R03/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_HC_2_s12_e1 ${MAINDIR}/R03/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R03/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R03/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R03/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R03/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R03/sess1/T1
 
-fslroi ${NIIDIR}/${r}/${s}/nii/${h}/srHC_e1_norm.nii ${r}/${s}/nii/${h}/BH_analysis/cropped/srHC_e1_norm_cropped 13 76
-fslroi ${r}/${s}/nii/${h}/srHC_e2_norm.nii ${r}/${s}/nii/${h}/BH_analysis/cropped/srHC_e2_norm_cropped 13 76
+#For R03 sess2, the 3rd HC run was for Clarisse, not for us.
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R03/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R03/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_BH_s18_e2 ${MAINDIR}/R03/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_BH_s18_e1 ${MAINDIR}/R03/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_HC_s8_e2 ${MAINDIR}/R03/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_HC_s8_e1 ${MAINDIR}/R03/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_HC_s12_e2 ${MAINDIR}/R03/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_HC_s12_e1 ${MAINDIR}/R03/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_RS_s6_e2 ${MAINDIR}/R03/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R03/sess2/ep2d_DE_pcasl_RS_s6_e1 ${MAINDIR}/R03/sess2/nii/RS_e1
+fslmaths ${NIIDIR}/R03/sess2/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R03/sess2/T1
+
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R05/sess1/nii/BH1_e2
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R05/sess1/nii/BH1_e1
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R05/sess1/nii/BH2_e2
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R05/sess1/nii/BH2_e1
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R05/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R05/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R05/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R05/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R05/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R05/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R05/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R05/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R05/sess1/T1
+
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R05/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R05/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R05/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R05/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R05/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R05/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R05/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R05/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R05/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R05/sess2/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R05/sess2/nii/RS_e1
+fslmaths ${NIIDIR}/R05/sess2/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R05/sess2/T1
+
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_BH_s12_e2 ${MAINDIR}/R06/sess1/nii/BH1_e2
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_BH_s12_e1 ${MAINDIR}/R06/sess1/nii/BH1_e1
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_BH_s18_e2 ${MAINDIR}/R06/sess1/nii/BH2_e2
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_BH_s18_e1 ${MAINDIR}/R06/sess1/nii/BH2_e1
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R06/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R06/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_HC_s14_e2 ${MAINDIR}/R06/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_HC_s14_e1 ${MAINDIR}/R06/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R06/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R06/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R06/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R06/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R06/sess1/T1
+
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_BH_s17_e2 ${MAINDIR}/R06/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_BH_s17_e1 ${MAINDIR}/R06/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_BH_s19_e2 ${MAINDIR}/R06/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_BH_s19_e1 ${MAINDIR}/R06/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_HC_s7_e2 ${MAINDIR}/R06/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_HC_s7_e1 ${MAINDIR}/R06/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_HC_s13_e2 ${MAINDIR}/R06/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_HC_s13_e1 ${MAINDIR}/R06/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_RS_s5_e2 ${MAINDIR}/R06/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R06/sess2/ep2d_DE_pcasl_RS_s5_e1 ${MAINDIR}/R06/sess2/nii/RS_e1
+fslmaths ${NIIDIR}/R06/sess2/MPRAGE_ADNI_Ipat2_s3_e1 ${MAINDIR}/R06/sess2/T1
+
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R07/sess1/nii/BH1_e2
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R07/sess1/nii/BH1_e1
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R07/sess1/nii/BH2_e2
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R07/sess1/nii/BH2_e1
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R07/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R07/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R07/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R07/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R07/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R07/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R07/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R07/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R07/sess1/T1
+
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_BH_s18_e2 ${MAINDIR}/R07/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_BH_s18_e1 ${MAINDIR}/R07/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_BH_s20_e2 ${MAINDIR}/R07/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_BH_s20_e1 ${MAINDIR}/R07/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R07/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R07/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_HC_s14_e2 ${MAINDIR}/R07/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_HC_s14_e1 ${MAINDIR}/R07/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_RS_s8_e2 ${MAINDIR}/R07/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R07/sess2/ep2d_DE_pcasl_RS_s8_e1 ${MAINDIR}/R07/sess2/nii/RS_e1
+fslmaths ${NIIDIR}/R07/sess2/MPRAGE_ADNI_Ipat2_s6_e1 ${MAINDIR}/R07/sess2/T1
+
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R08/sess1/nii/BH1_e2
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R08/sess1/nii/BH1_e1
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R08/sess1/nii/BH2_e2
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R08/sess1/nii/BH2_e1
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R08/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R08/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R08/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R08/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R08/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R08/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R08/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R08/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R08/sess1/T1
+
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R08/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R08/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R08/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R08/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R08/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R08/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R08/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R08/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R08/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R08/sess2/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R08/sess2/nii/RS_e1
+fslmaths ${NIIDIR}/R08/sess2/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R08/sess2/T1
+
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R09/sess1/nii/BH1_e2
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R09/sess1/nii/BH1_e1
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R09/sess1/nii/BH2_e2
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R09/sess1/nii/BH2_e1
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R09/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R09/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R09/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R09/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R09/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R09/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R09/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R09/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R09/sess1/T1
+
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R09/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R09/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R09/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R09/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R09/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R09/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R09/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R09/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R09/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R09/sess2/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R09/sess2/nii/RS_e1
+fslmaths ${NIIDIR}/R09/sess2/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R09/sess2/T1
+
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_BH_s14_e2 ${MAINDIR}/R10/sess1/nii/BH1_e2
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_BH_s14_e1 ${MAINDIR}/R10/sess1/nii/BH1_e1
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_BH_s16_e2 ${MAINDIR}/R10/sess1/nii/BH2_e2
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_BH_s16_e1 ${MAINDIR}/R10/sess1/nii/BH2_e1
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_HC_s6_e2 ${MAINDIR}/R10/sess1/nii/HC1_e2
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_HC_s6_e1 ${MAINDIR}/R10/sess1/nii/HC1_e1
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_HC_s10_e2 ${MAINDIR}/R10/sess1/nii/HC2_e2
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_HC_s10_e1 ${MAINDIR}/R10/sess1/nii/HC2_e1
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_RS_s4_e2 ${MAINDIR}/R10/sess1/nii/RS_e2
+fslmaths ${NIIDIR}/R10/sess1/ep2d_DE_pcasl_RS_s4_e1 ${MAINDIR}/R10/sess1/nii/RS_e1
+fslmaths ${NIIDIR}/R10/sess1/MPRAGE_ADNI_Ipat2_s2_e1 ${MAINDIR}/R10/sess1/T1
+
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_BH_s15_e2 ${MAINDIR}/R10/sess2/nii/BH1_e2
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_BH_s15_e1 ${MAINDIR}/R10/sess2/nii/BH1_e1
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_BH_s17_e2 ${MAINDIR}/R10/sess2/nii/BH2_e2
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_BH_s17_e1 ${MAINDIR}/R10/sess2/nii/BH2_e1
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_HC_s7_e2 ${MAINDIR}/R10/sess2/nii/HC1_e2
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_HC_s7_e1 ${MAINDIR}/R10/sess2/nii/HC1_e1
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_HC_s11_e2 ${MAINDIR}/R10/sess2/nii/HC2_e2
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_HC_s11_e1 ${MAINDIR}/R10/sess2/nii/HC2_e1
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_RS_s5_e2 ${MAINDIR}/R10/sess2/nii/RS_e2
+fslmaths ${NIIDIR}/R10/sess2/ep2d_DE_pcasl_RS_s5_e1 ${MAINDIR}/R10/sess2/nii/RS_e1
+#series 3 MPRAGE has motion
+fslmaths ${NIIDIR}/R10/sess2/MPRAGE_ADNI_Ipat2_s20_e1 ${MAINDIR}/R10/sess2/T1
+
+
+
---
+++ crop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MAINDIR=/Users/erin/Desktop/Projects/Repeatability/working
+
+
+for r in R02 R03 R05 R06 R07 R08 R09 R10
+do 
+for s in sess1 sess2
+do 
+#crop HC and RS runs so they are the same length as the BH runs
+for run in HC1_e1 HC1_e2 HC2_e1 HC2_e2 RS_e1 RS_e2
+do
+fslroi ${MAINDIR}/${r}/${s}/nii/${run} {MAINDIR}/${r}/${s}/nii/${run}_cropped 13 76
+done
+
+#crop ET data to match
+for run in hc1_1 hc2_1
+do
+#get rid of spaces
+sed 's/ //g' ${MAINDIR}/${r}/${s}/respiract/${run}.txt > ${MAINDIR}/${r}/${s}/respiract/${run}_sed.txt
+#extract last 76 values
+cut -f 15-90  ${MAINDIR}/${r}/${s}/respiract/${run}_sed.txt >  ${MAINDIR}/${r}/${s}/respiract/${run}_sed_cropped.txt 
+#scale such that min=0
+min=`awk '{m=$1;for(i=1;i<=NF;i++)if($i<m)m=$i;print m}' ${MAINDIR}/${r}/${s}/respiract/${run}_sed_cropped.txt`
+awk -v min=$min '{for(i=1;i<NF;i++)print $i-min}' ${MAINDIR}/${r}/${s}/respiract/${run}_sed_cropped.txt > ${MAINDIR}/${r}/${s}/respiract/${run}_sed_cropped_0.txt
+done
+
+done
+done
+
+
+
+
