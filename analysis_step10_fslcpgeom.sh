f3f9af30676e20598f3be439fa53299fed31eb6f
Erin Mazerolle <erinmaz@gmail.com>
2018-04-20 09:08:13 -0600

    fslcpgeom wrapper

---
+++ analysis_step10_fslcpgeom.sh
@@ -0,0 +1,11 @@
+cd /Users/erin/Desktop/Projects/Repeatability/working
+for f in `find . -name "*mip*"`
+do
+fslcpgeom /Users/erin/Desktop/Projects/Repeatability/working/R02/sess1/nii/meanBH1_e1_cropped_brain_mask.nii $f
+done
+
+cd /Users/erin/Desktop/Projects/Repeatability/working
+for f in `find . -name "*delay*"`
+do
+fslcpgeom /Users/erin/Desktop/Projects/Repeatability/working/R02/sess1/nii/meanBH1_e1_cropped_brain_mask.nii $f
+done
