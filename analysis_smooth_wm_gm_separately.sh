932056a23abf52740b3e1922694b8fae33aa2923
Erin Mazerolle <erinmaz@gmail.com>
2018-05-18 15:16:15 -0600

    before major overhaul. writing methods first

--- analysis_smooth_wm_gm_separately.sh
+++ analysis_smooth_wm_gm_separately.sh
@@ -7,8 +7,11 @@ do
 #for run in BH1 BH2 HC1 HC2 RS
 for run in RS
 do
+
+#need to exclude few rare voxels that end up in both masks (a couple in the cerebellum for R02 sess1 RS)
 fslmaths ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped -nan -mas c1T1.8_to_RS.nii.gz -kernel gauss 5 -fmedian ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped_gm_gauss5_fmedian
 fslmaths ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped -nan -mas c2T1.8_to_RS.nii.gz -kernel gauss 5 -fmedian ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped_wm_gauss5_fmedian
+
 fslmaths ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped_gm_gauss5_fmedian -add ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped_wm_gauss5_fmedian ${MAINDIR}/${sub}/${sess}/nii/rRS_e1_cropped_gm_wm_gauss5_fmedian
 done
 done
