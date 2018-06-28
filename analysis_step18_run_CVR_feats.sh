274b0be837e73b6ef1675c3bc4474352fe1bcce7
Erin Mazerolle <erinmaz@gmail.com>
2018-06-28 09:40:19 -0600

    updated step18 to include fdr for e2 feats

--- analysis_step18_run_CVR_feats.sh
+++ analysis_step18_run_CVR_feats.sh
@@ -3,72 +3,80 @@
 MAINDIR=/Users/erin/Desktop/Projects/Repeatability/working
 q=0.05 #for FDR correction
 
-#manually run in GUI for bh1_09
+#have already manually run in GUI for bh1_09
+e2dof=`cat ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/stats/dof`
+fdr_cor.sh ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/stats $q $e2dof
+
 for run in bh1_10 bh1_11 bh1_12 bh1_13 bh1_14 bh1_15 bh1_16 bh1_17 bh1_18 bh1_19 bh1_20 
 do
-sed 's:bh1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/design.fsf > ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:bh1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/design.fsf > ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
+
 cp ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/design.fsf ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.fsf
 
 sed 's:BH1:BH2:g' ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/design.fsf > ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_bh1_09.fsf
 
 for run in bh2_09 bh2_10 bh2_11 bh2_12 bh2_13 bh2_14 bh2_15 bh2_16 bh2_17 bh2_18 bh2_19 bh2_20 
 do
-sed 's:bh1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_bh1_09.fsf > ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:bh1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_bh1_09.fsf > ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
+
 rm ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_bh1_09.fsf
 
 for run in bh1_09 bh1_10 bh1_11 bh1_12 bh1_13 bh1_14 bh1_15 bh1_16 bh1_17 bh1_18 bh1_19 bh1_20 
 do
-sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess2/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess2/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess2/nii/rBH1_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
 
 for run in bh2_09 bh2_10 bh2_11 bh2_12 bh2_13 bh2_14 bh2_15 bh2_16 bh2_17 bh2_18 bh2_19 bh2_20 
 do
-sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess2/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess2/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess2/nii/rBH2_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
 
 sed 's:BH1:HC1:g' ${MAINDIR}/R02/sess1/nii/rBH1_e2_cropped_nan_gm_sm_bh1_09.feat/design.fsf > ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_bh1_09.fsf
 
 for run in hc1_09 hc1_10 hc1_11 hc1_12 hc1_13 hc1_14 hc1_15 hc1_16 hc1_17 hc1_18 hc1_19 hc1_20 
 do
-sed 's:bh1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_bh1_09.fsf > ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:bh1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_bh1_09.fsf > ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
+
 rm ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_bh1_09.fsf
 
 sed 's:HC1:HC2:g' ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_hc1_09.fsf > ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_hc1_09.fsf 
 
 for run in hc2_09 hc2_10 hc2_11 hc2_12 hc2_13 hc2_14 hc2_15 hc2_16 hc2_17 hc2_18 hc2_19 hc2_20 
 do
-sed 's:hc1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_hc1_09.fsf  > ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf 
-feat ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:hc1_09:'${run}':g' ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_hc1_09.fsf  > ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf 
+  feat ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
+
 rm ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_hc1_09.fsf
 
 for run in hc1_09 hc1_10 hc1_11 hc1_12 hc1_13 hc1_14 hc1_15 hc1_16 hc1_17 hc1_18 hc1_19 hc1_20 
 do 
-sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess2/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess2/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
+  fdr_cor.sh ${MAINDIR}/R02/sess2/nii/rHC1_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
 
 for run in hc2_09 hc2_10 hc2_11 hc2_12 hc2_13 hc2_14 hc2_15 hc2_16 hc2_17 hc2_18 hc2_19 hc2_20 
 do 
-sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/R02/sess2/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:sess1:sess2:g' ${MAINDIR}/R02/sess1/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/R02/sess2/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/R02/sess2/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/R02/sess2/nii/rHC2_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof
 done
-wait
+
 
 for r in R03 R05 R06 R07 R08 R09 R10
 do
@@ -76,33 +84,34 @@ for s in sess1 sess2
 do
 for run in hc1_09 hc1_10 hc1_11 hc1_12 hc1_13 hc1_14 hc1_15 hc1_16 hc1_17 hc1_18 hc1_19 hc1_20 
 do 
-sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/${r}/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/${r}/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/${r}/${s}/nii/rHC1_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof 
 done
-wait
 
 for run in hc2_09 hc2_10 hc2_11 hc2_12 hc2_13 hc2_14 hc2_15 hc2_16 hc2_17 hc2_18 hc2_19 hc2_20 
 do 
-sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/${r}/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/${r}/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/${r}/${s}/nii/rHC2_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof 
 done
-wait
 
 for run in bh1_09 bh1_10 bh1_11 bh1_12 bh1_13 bh1_14 bh1_15 bh1_16 bh1_17 bh1_18 bh1_19 bh1_20 
 do
-sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/${r}/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/${r}/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/${r}/${s}/nii/rBH1_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof 
 done
-wait
 
 for run in bh2_09 bh2_10 bh2_11 bh2_12 bh2_13 bh2_14 bh2_15 bh2_16 bh2_17 bh2_18 bh2_19 bh2_20 
 do
-sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf
-feat ${MAINDIR}/${r}/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf &
+  sed 's:R02:'${r}':g' ${MAINDIR}/R02/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf > ${MAINDIR}/${r}/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf
+  feat ${MAINDIR}/${r}/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.fsf 
+  fdr_cor.sh ${MAINDIR}/${r}/${s}/nii/rBH2_e2_cropped_nan_gm_sm_${run}.feat/stats $q $e2dof 
 done
-wait
 
 done
+
 done
 
 run=bh1_09 #feat run manually
